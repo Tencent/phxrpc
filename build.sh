754b0cec408c220b0361bb281e4d11ff3c070446
unixliang <unixliang@tencent.com>
2019-10-21 15:03:24 +0800

    add boost in build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd third_party;
 if [ ! -f protobuf/bin/protoc ]; then
 	if [ ! -f protobuf-cpp-3.0.0.tar.gz ]; then
 		wget https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-cpp-3.0.0.tar.gz
-	fi	
+	fi
 
 	tar zxvf protobuf-cpp-3.0.0.tar.gz
 	cd protobuf-3.0.0
@@ -20,6 +20,18 @@ if [ ! -f protobuf/bin/protoc ]; then
 
 	cd ../
 fi
+
+
+if [ ! -d boost ]; then
+	if [ ! -f boost_1_58_0.tar.gz ]; then
+		wget https://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz
+	fi
+
+	tar zxvf boost_1_58_0.tar.gz
+  mv boost_1_58_0 boost
+  export BOOST_ROOT=`pwd`/boost
+fi
+
 
 cd ..
 
